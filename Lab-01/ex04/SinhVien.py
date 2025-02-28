a9aac10bc8e7e6ea2bf8432d45359cf71db277fc
NguyenTranTrungHieu <trunghieu732003@gmail.com>
2025-02-28 10:55:22 +0700

    [add] lab-01 ex04 and .gitignore

---
+++ Lab-01/ex04/SinhVien.py
@@ -0,0 +1,8 @@
+class SinhVien:
+    def __init__(self, svId, name, sex, major, diemTB):
+        self._id = svId
+        self._name = name
+        self._sex = sex
+        self._major = major
+        self._diemTB = diemTB
+        self._hocLuc = None
